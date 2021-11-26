2b64ea946d41f844d0129053d40bf0ddbb87cc11
apavankumar <apavankumar@kluniversity.in>
2021-11-26 12:57:21 +0530

    Intial modified commit

---
+++ app/src/main/java/WordDao.kt
@@ -0,0 +1,21 @@
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface WordDao {
+
+    @Query("SELECT * FROM word_table ORDER BY word ASC")
+//    fun getAlphabetizedWords(): List<Word>
+//   Observing database changes modify the above function
+    fun getAlphabetizeWords():Flow<List<Word>>
+
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(word: Word)
+
+    @Query("DELETE FROM word_table")
+    suspend fun deleteAll()
+}
